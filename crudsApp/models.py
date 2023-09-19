d7b7c096743e04f0fa42b0c1b259d4eedaeaa11d
SS COMPUTER <jahidjesan508@gmail.com>
2023-09-19 20:23:23 +0600

    update

--- crudsApp/models.py
+++ crudsApp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 
 class profile(models.Model):
+    objects = None
     RELIGION = (
         ('Islam', 'Islam'),
         ('Hindu', 'Hindu'),
@@ -38,13 +39,14 @@ class profile(models.Model):
     Photo = models.ImageField(upload_to='prof_pic/')
     Address = models.TextField(max_length=40)
     Contact_numbers = models.TextField(max_length=15)
-    Email_Address = models.EmailField(max_length=20)
+    Email_Address = models.EmailField(max_length=30)
     Date_of_Birth = models.DateField()
     Religion = models.CharField(choices=RELIGION, max_length=20)
     Nationality = models.TextField(max_length=20)
     Marital_status = models.CharField(choices=MARITAL_STATUS, max_length=15)
     gender = models.CharField(choices=GENDER, max_length=10, default='Male')
     blood_group = models.CharField(choices=BLOOD_GROUP, max_length=8, default='A+')
+    is_alive = models.BooleanField(default=True, null=True, blank=True)
 
     def __str__(self):
         return self.Name
--- crudsApp/views.py
+++ crudsApp/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 
 def index(request):
+    # try to first way >>>>>>>>>>
+    # total_profile = profile.objects.all()
+    # for i in total_profile:
+    #     print(f'profile is : {i} phone number is :{i.Contact_numbers} and Email is : {i.Email_Address} ')
+
+    # try to 2nd wey >>>>>>>>>>>  All data show to backend terminal
+    # total_profile = profile.objects.filter(is_alive=False)
+    # for i in total_profile:
+    #     print(f'profile is : {i} phone number is :{i.Contact_numbers} and Email is : {i.Email_Address} ')
+
+    # try to 3rd wey >>>>>>>>>>>  Robot Adrita fixing data backend output show ?
+    # total_profile = profile.objects.get(Name= "Robot Adrita")
+    # print(total_profile.Email_Address)
+
     return render(request, "index.html")
