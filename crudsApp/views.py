54dccc7ed139e606e815f7ad94483edd45c960c5
SS COMPUTER <jahidjesan508@gmail.com>
2023-09-20 00:16:39 +0600

    update

--- crudsApp/views.py
+++ crudsApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 
+
 # Create your views here.
 
 
@@ -19,4 +20,14 @@ def index(request):
     # total_profile = profile.objects.get(Name= "Robot Adrita")
     # print(total_profile.Email_Address)
 
-    return render(request, "index.html")
+    # backend unit data press to add all info >>>>>?
+    # prof = profile.objects.create(Name="Rana", Address="mymensing,valuka", Contact_numbers="01798845625",
+    #                               Email_Address="rana@gmail.com", Date_of_Birth="1996-09-23",
+    #                               Religion="Islam", Nationality="Bangladeshi", gender="Female", blood_group="AB-",
+    #                               is_alive=False)
+    # prof.save()
+
+    # use to all() and get() and filter() three functions/method
+    # prof = profile.objects.get(Name="Robot Adrita")
+    prof = profile.objects.all()
+    return render(request, "index.html", locals())
